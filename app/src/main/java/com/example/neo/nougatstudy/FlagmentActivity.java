861f6006022b51d5064164d4441270a9d32a4fe5
neo <mcvzone@naver.com>
2017-07-17 10:59:00 +0900

    fragment, tab added

--- app/src/main/java/com/example/neo/nougatstudy/FlagmentActivity.java
+++ app/src/main/java/com/example/neo/nougatstudy/FlagmentActivity.java
@@ -1,5 +1,7 @@
 package com.example.neo.nougatstudy;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -25,6 +27,45 @@ public class FlagmentActivity extends AppCompatActivity {
 //        listFragment = (ListFragment)fm.findFragmentById(R.id.FRAGMENT_FL_CONTAINER);
 //        viewerFragment = (ViewerFragment)fm.findFragmentById(R.id.FRAGMENT_FL_CONTAINER2);
 
+        TabLayout tabs = (TabLayout)findViewById(R.id.FRAGMENT_TL_TAB);
+        tabs.addTab(tabs.newTab().setText("친구"));
+        tabs.addTab(tabs.newTab().setText("일대일채팅"));
+        tabs.addTab(tabs.newTab().setText("기타"));
+
+        tabs.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                int position = tab.getPosition();
+
+                Fragment selected = null;
+
+                if (position == 0) {
+                    selected = mainfragment;
+                    getSupportFragmentManager().beginTransaction().replace(R.id.FRAGMENT_FL_CONTAINER, selected).commit();
+                } else if (position == 1) {
+                    selected = menufragment;
+                    getSupportFragmentManager().beginTransaction().replace(R.id.FRAGMENT_FL_CONTAINER, selected).commit();
+                } else if (position == 2) {
+                    selected = listFragment;
+                    getSupportFragmentManager().beginTransaction().replace(R.id.FRAGMENT_FL_CONTAINER, selected).commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.FRAGMENT_FL_CONTAINER2, viewerFragment).commit();
+                }
+
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        /*
         findViewById(R.id.FRAGMENT_BT_START).setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
@@ -45,7 +86,7 @@ public class FlagmentActivity extends AppCompatActivity {
                 getSupportFragmentManager().beginTransaction().replace(R.id.FRAGMENT_FL_CONTAINER, listFragment).commit();
                 getSupportFragmentManager().beginTransaction().replace(R.id.FRAGMENT_FL_CONTAINER2, viewerFragment).commit();
             }
-        });
+        });*/
 
     }
 
@@ -59,6 +100,7 @@ public class FlagmentActivity extends AppCompatActivity {
     }
 
     public void onChangeImage(int index){
+        getSupportFragmentManager().beginTransaction().replace(R.id.FRAGMENT_FL_CONTAINER2, viewerFragment).commit();
         viewerFragment.setImage(index);
     }
 }
--- app/src/main/java/com/example/neo/nougatstudy/MainActivity.java
+++ app/src/main/java/com/example/neo/nougatstudy/MainActivity.java
@@ -11,6 +11,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutCompat;
@@ -33,6 +34,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //ActionBar abar = getSupportActionBar();
+        //abar.hide();
 
         int permissionReceiveSms = ContextCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS);
 
